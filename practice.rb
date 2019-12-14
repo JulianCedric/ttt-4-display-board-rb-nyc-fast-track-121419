d53122ea3be1d8d4b8ab8b200a88381aaee74a18
Julian Pormentilla <julian.pormentilla@gmail.com>
2019-12-14 23:28:28 +0000

    Automatically backed up by Learn

---
+++ practice.rb
@@ -0,0 +1,52 @@
+# # # practice 
+
+# # def say_hello(name = "Ruby Programmer")
+# #   puts "Hello #{name}!"
+# # end
+
+# # say_hello
+
+# # say_hello("Sophie")
+
+# # puts " "
+
+# list1 = [1,2,3]
+
+# list2 = [4,5,6]
+
+# puts list2 << 7
+
+# puts list2
+
+# # list3 = [list1, list2]
+
+# # # puts list3.length
+
+# # puts list1.length
+
+# # puts list3[1][2]
+
+# # # puts list2[-1]
+
+# # puts list2.length[1]
+
+# # puts list2.length 
+
+famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+lil_bub = famous_cats.unshift 
+
+# puts lil_bub
+
+# puts famous_cats.inspect 
+
+# puts famous_cats 
+
+# puts lil_bub
+
+puts lil_bub.inspect
+
+
+
+
+
+
